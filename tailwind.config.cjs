6183e3f060833f3fa94febbbf8832b3ccb9e6097
Zeu Capua <zeucapua@gmail.com>
2023-03-21 16:24:49 -0700

    finished svelte-auth tutorial, restyle to cupcake

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,21 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-    colors: {
-      "base": "#d8cdcd",
-      "content": "#48322e",
-      "accent": "#ad8a85",
-    },
 		extend: {
       fontFamily: {
         bebasneue: ["BebasNeue"],
         nunito: ["Nunito"],
       },
-      typography: {
-        DEFAULT: {
-          css: { color: "#48322e", }
-        }
-      }
     },
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")]
